8f9f0b90c51dba8160404e118fdbb11f948d4f47
natalyVerdugoNogue <nataly.verdugo.nogue@gmail.com>
2018-07-25 13:10:02 -0400

    imagenes

---
+++ src/js/controller.js
@@ -0,0 +1,8 @@
+// window.onload = () => {
+//   setTimeout(() => prueba(), 3000);
+// };
+
+// const prueba = () => {
+//   let divPrueba = document.getElementById('counter');
+//   divPrueba.innerHTML = `<p>funca</p>`
+// };
